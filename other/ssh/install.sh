49ee763a7395a5a81a127d9780251f31c29f59eb
シングル‬‏ <39969445+MN-HACKER@users.noreply.github.com>
2025-05-22 18:57:10 +0330

    Update install.sh

--- other/ssh/install.sh
+++ other/ssh/install.sh
@@ -2,7 +2,7 @@ source /opt/hiddify-manager/common/utils.sh
 latest=$(get_release_version ssh-liberty-bridge)
 
 if [ "$(cat VERSION 2>/dev/null)" != $latest ]; then
-    curl -sL -o ssh-liberty-bridge https://github.com/hiddify/ssh-liberty-bridge/releases/latest/download/ssh-liberty-bridge-$(dpkg --print-architecture)
+    curl -sL -o ssh-liberty-bridge https://github.com/hiddify/ssh-liberty-bridge/releases/download/$latest/ssh-liberty-bridge-$(dpkg --print-architecture)
     chmod +x ssh-liberty-bridge
     echo $latest >VERSION
     useradd liberty-bridge
