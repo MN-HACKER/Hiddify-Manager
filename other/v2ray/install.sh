f92927022ac43e818fff8996dfc9b36f8feba5ca
シングル‬‏ <39969445+MN-HACKER@users.noreply.github.com>
2025-05-22 18:53:54 +0330

    Update install.sh

--- other/v2ray/install.sh
+++ other/v2ray/install.sh
@@ -2,7 +2,7 @@ source /opt/hiddify-manager/common/utils.sh
 install_package shadowsocks-libev
 
 if ! is_installed ./v2ray-plugin_linux; then
-    wget -c https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.2/v2ray-plugin-linux-$(dpkg --print-architecture)-v1.3.2.tar.gz
-    tar xvzf v2ray-plugin-linux-*
-    mv v2ray-plugin_linux_$(dpkg --print-architecture) v2ray-plugin_linux
+    wget -c https://github.com/v2fly/v2ray-core/releases/download/v5.32.0/v2ray-linux-$(dpkg --print-architecture)-v5.32.0.tar.gz
+    tar xvzf v2ray-linux-$(dpkg --print-architecture)-v5.32.0.tar.gz
+    mv v2ray v2ray_linux
 fi
