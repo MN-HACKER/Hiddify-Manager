2db68650e389dc51ea7f6af0737b2d0d52f6bbdb
シングル‬‏ <39969445+MN-HACKER@users.noreply.github.com>
2025-05-29 21:23:53 +0330

    Update download.sh

--- common/download.sh
+++ common/download.sh
@@ -2,11 +2,9 @@
 
 if [[ "$VER" != "" ]];then
     set -- $VER  $@
-
 fi
 
 echo "$0 input params are $@"
-
 
 if [[ " $@ " != *"--no-gui"* ]] &&  [[ "$0" == "bash" ]]; then
     echo "This script is deprecated! Please use the following command"
@@ -19,14 +17,13 @@ fi
 echo "Downloading '$@'"
 
 if [[ " $@ " == *" v8 "* ]]; then
-    sudo bash -c "$(curl -sLfo- https://raw.githubusercontent.com/hiddify/hiddify-config/main/common/download_install.sh)"
+    sudo bash -c "$(curl -sLfo- https://raw.githubusercontent.com/MN-HACKER/hiddify-config/main/common/download_install.sh)"
     exit $?
 fi
 
-
 mkdir -p /tmp/hiddify/
-curl -sL -o /tmp/hiddify/hiddify_installer.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/hiddify_installer.sh
-curl -sL -o /tmp/hiddify/utils.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/utils.sh
+curl -sL -o /tmp/hiddify/hiddify_installer.sh https://raw.githubusercontent.com/MN-HACKER/Hiddify-Manager/main/common/hiddify_installer.sh
+curl -sL -o /tmp/hiddify/utils.sh https://raw.githubusercontent.com/MN-HACKER/Hiddify-Manager/main/common/utils.sh
 chmod +x /tmp/hiddify/hiddify_installer.sh
 chmod +x /tmp/hiddify/utils.sh
 /tmp/hiddify/hiddify_installer.sh $@
